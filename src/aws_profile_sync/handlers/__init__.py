ace2e706f3964965372c539b050cad18dd1011b1
Felddy <markf@geekpad.com>
2020-02-10 17:06:02 -0500

    Add documentation to module helper function.

--- src/aws_profile_sync/handlers/__init__.py
+++ src/aws_profile_sync/handlers/__init__.py
@@ -5,6 +5,15 @@ __all__ = ["SSHGitHandler"]
 
 
 def find_handler(url):
+    """Find a handler that will support a URL.
+
+    Args:
+        url: The URL to find a handler for.
+
+    Returns:
+        If a handler is found a class will be returned, None otherwise.
+
+    """
     for handler in __all__:
         # Get the symbol for handler
         mod = globals()[handler]
